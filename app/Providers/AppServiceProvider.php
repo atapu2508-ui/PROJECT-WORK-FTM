ffad08031c61ba955a8866db7e9b991b5cfa98cf
lukashirikato <farhanrizkysaputra2508@gmail.com>
2026-05-23 19:49:15 +0700

    force https railway

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Filament\Facades\Filament;
 use Midtrans\Config;
 use App\Models\Customer;
@@ -33,6 +34,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+
+         if (env('APP_ENV') === 'production') {
+        URL::forceScheme('https');
+    }
+    
         // Midtrans Configuration
         Config::$serverKey     = config('midtrans.server_key');
         Config::$clientKey     = config('midtrans.client_key');
